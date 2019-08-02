719d35e44bc6fa6e8d4f0ccf6082630db9aa7847
Danil <danil-babich@yandex.ru>
2019-08-02 10:34:28 +0300

    Add ID copmare in Modify and Create tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTestVer2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTestVer2.java
@@ -5,7 +5,10 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTestVer2 extends TestBase {
 
@@ -13,13 +16,26 @@ public class GroupCreationTestVer2 extends TestBase {
   public void testGroupCreationTestVer2() {
     app.getNavigationHelper().gotoGroupPage("groups");
     List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("Test", "SomeHeader", "SomeText");
     app.getGroupHelper().initGroupCreation("new");
-    app.getGroupHelper().fillGroupForm(new GroupData("Test", "SomeHeader", "SomeText"));
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().sumbitGroupCreation("submit");
     //app.getNavigationHelper().returntoGroupPage("home page");
     app.getNavigationHelper().gotoGroupPage("groups");
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size()+1); // Важно следить где считаются элементы!!
+
+    int max = 0;
+    for (GroupData g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+
+    assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,10 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTests extends TestBase {
 
@@ -18,11 +20,16 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification("edit");
-    app.getGroupHelper().fillGroupForm(new GroupData("NewGroupForKatalon", "TestText", "SomeText"));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(),"test1", "TestText", "SomeText");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().sumbitGroupModification("update");
     app.getNavigationHelper().returntoGroupPage("home page");
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size());
+    assertEquals(after.size(), before.size());
+
+    before.remove(before.size()-1);
+    before.add(group);
+    assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
 
